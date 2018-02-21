5a5883aa8c94f662bcee9d2dbc9b5d51fb5d37ee
jinhaoying <35727900+jinhaoying@users.noreply.github.com>
2018-02-20 23:51:40 -0800

    Update smoke-arrays.js

--- smoke-arrays.js
+++ smoke-arrays.js
@@ -1,9 +1,20 @@
-var x = 210;
-var y = 290;
+var x = []
+var y = [];
 var r = 0;
+var x1 = [];
+var y1 = [];
+var r1 = []
+
 
 function setup() {
   createCanvas(400, 400);
+	for (var i = 0; i < 100; i = i + 1) {
+		x1[i] = random(-30,20);
+		y1[i] = random (-40,30);
+		r1[i] = random (0,5)*0.01;
+		x[i] = 210;
+		y[i] = 280;
+	}
 }
   
 function draw() {
@@ -15,25 +26,37 @@ function draw() {
   rect(195, height, 30, -100);
 
   // save graphics state
-  push();
-  // darker as it gets closer to 0
-  fill(y);
-  // rotate r around (x,y)
-  translate(x, y);
-  rotate(r);
-  // draw rectangle
-  rect(-10, -10, 20, 20);
-  // reset rotation and translation
-  pop();
-  
-  // up 3 pixels
-  y -= 3;
-  
-  // rotate 0.05 radians ~= 2.8 degrees per frame
-  r += 0.05
+	for (var i = 0; i < 100; i = i + 1) {
+  	push();
+  	// darker as it gets closer to 0
+  	fill(y[i]);
+ 	 	// rotate r around (x,y)
+ 	 	translate(x[i], y[i]);
+ 	 	rotate(r1[i]);
+  	// draw rectangle
+  //	rect(-10, -10, 20, 20);
+		rect(x1[i], y1[i], 20, 20);
+  	// reset rotation and translation
+  	pop();
+
+  	// up 3 pixels
+  	y[i] -= random(0,50)*0.1;
+		if(random() < 0.50){
+			x[i] -= random(0,10);
+		}
+		else{
+			x[i] += random(0,10);
+		}
+	
+  	// rotate 0.05 radians ~= 2.8 degrees per frame
+  	r1[i] += 0.01+random(0,5)*0.1;
+	
   
   // if reach past the top a bunch
-  if (y < -150) {
-    y = 290;
-  }
+	
+  	if (y[i] < -150) {
+    	y[i] = 280;
+			x[i]=210;
+  	}
+	}	
 }
